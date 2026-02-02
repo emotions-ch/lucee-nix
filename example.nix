7c3f9425fd95b45ff6f47e6e5740e51dae7290a2
Ori <meow@ori.cat>
2026-02-02 21:55:00 +0100

    build service after dockerfiles

--- example.nix
+++ example.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ lib, config, pkgs, ... }:
 let
   lucee = pkgs.stdenv.mkDerivation {
     name = "lucee-7.0.1.100";
@@ -13,83 +13,67 @@ let
     '';
   };
 
-  serverXml = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/lucee/lucee-installer/6c16fd305c22cd2022c1f64e7ab31dca1bdb3112/lucee/tomcat9/tomcat-lucee-conf/conf/server.xml"; #server.xml from lucee7 installer
-    sha256 = "0ppizzx1q2lpfl8mn3bhaf145dj38pmj0cpgax21jx68fcy98m8s";
-  };
-
-  contextXml = pkgs.stdenv.mkDerivation {
-    name = "lucee-contextXml";
-    src = pkgs.fetchurl {
-      url = "https://github.com/lucee/lucee-installer/raw/0d5e993ba99aa7ce4b64b1d26e73e250b9b30a3b/lucee/tomcat9/tomcat-lucee-conf/conf/context.xml"; #context.xml from lucee7 installer
-      sha256 = "04ldddb8pq7y3y5yggz29ghq9i08a8jgs4v12is0hhppm9b23hir";
-    };
-
-    dontUnpack = true;
-
-    installPhase = ''
-      mkdir -p $out
-      cp $src $out/context.xml
-    '';
-  };
-
-  webXml = pkgs.stdenv.mkDerivation {
-    name = "lucee-webXml";
-    src = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/lucee/lucee-installer/a22896f9b0caef10ea19943ba6302f2499bda96c/lucee/tomcat9/tomcat-lucee-conf/conf/web.xml"; #web.xml from lucee7 installer
-      sha256 = "1kxg7yymi7gyzf3pa6yn521d86zmgn9mivy2skpn3hbfaa0qar9m";
-    };
-
-    dontUnpack = true;
-
-    installPhase = ''
-      mkdir -p $out
-      cp $src $out/web.xml
-    '';
-  };
-
-  catalinaProperties = pkgs.stdenv.mkDerivation {
-    name = "lucee-catalinaProperties-service-patch";
-    src = pkgs.fetchurl {
-      url = "https://github.com/lucee/lucee-installer/raw/860059147b84686b2d3db681cc30f216bc17c4f7/lucee/tomcat10/conf/catalina.properties"; # catalina.properties from lucee7 installer
-      sha256 = "1i9vbgg39gxzgn4zw3gbm5apfqchksvqssna1dg2jbd253xydllx";
-    };
-
-    dontUnpack = true;
-    buildInputs = [ pkgs.gnused ];
-
-    installPhase = ''
-      mkdir -p $out
-      cp $src $out/catalina.properties
-    '';
+  lucee-dockerfiles = pkgs.fetchFromGitHub {
+    owner = "lucee";
+    repo = "lucee-dockerfiles";
+    rev = "0b34c46e8c1385d7c4014ee6c154476a9995189d";
+    sha256 = "sha256-tzR30emegBC27E4XImgQrOBTYOGacaXmdvCZrvjLhsg=";
   };
 
   tomcat-lucee = pkgs.tomcat11.overrideAttrs (oldAttrs: {
     postInstall = (oldAttrs.postInstall or "") + ''
-      # config files for lucee which need to be writable by tomcat
-      cp -f ${catalinaProperties}/catalina.properties $out/conf/catalina.properties
+      cp -f ${lucee-dockerfiles}/config/tomcat/11.0/* $out/conf/
+      mkdir -p $out/lucee
+      cp ${lucee}/lucee.jar $out/lucee/
     '';
   });
 in
 {
   services.tomcat = {
     enable = true;
-    package = pkgs.tomcat11;
+    package = tomcat-lucee;
     jdk = pkgs.openjdk25;
-    commonLibs = [ "${lucee}/lucee.jar" ];
 
-    # serverXml = builtins.readFile serverXml;
-    extraConfigFiles = [
-      # "${contextXml}/context.xml"
-      # "${webXml}/web.xml"
+    webapps = ["${pkgs.fetchFromGitHub {
+      owner = "lucee";
+      repo = "lucee-installer";
+      rev = "da57a8ec6e2993ed873445dde2ce4ea8dc2c4bb8";
+      sha256 = "sha256-KqdDwzSy1r9jiIMVRJHyLaQgsdUk75TIOI9LsSISOII=";
+    }}/lucee/tomcat9/tomcat-lucee-conf"];
+  };
+
+  systemd = {
+    services = {
+      tomcat = {
+        after = [ "lucee-setup.service" ];
+        requires = [ "lucee-setup.service" ];
+        preStart = lib.mkAfter ''
+          ln -sfn ${config.services.tomcat.package}/lucee ${config.services.tomcat.baseDir}/lucee
+        '';
+      };
+
+      "lucee-setup" = {
+        description = "Initialize /opt/lucee for Lucee";
+        wantedBy = [ "multi-user.target" ];
+        before = [ "tomcat.service" ];
+        unitConfig.RequiresMountsFor = [ "/opt/lucee" ];
+
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = ''
+            ${pkgs.coreutils}/bin/mkdir -p /opt/lucee
+            ${pkgs.coreutils}/bin/chmod 0755 /opt/lucee
+            ${pkgs.coreutils}/bin/chown ${config.services.tomcat.user}:${config.services.tomcat.group} /opt/lucee
+          '';
+          RemainAfterExit = true;
+        };
+      };
+    };
+
+    tmpfiles.rules = [
+      # Format: d <path> <mode> <user> <group> <age or ->
+      "d /opt/lucee 0755 ${config.services.tomcat.user} ${config.services.tomcat.group} -"
     ];
-
-    # webapps = ["${pkgs.fetchFromGitHub {
-    #   owner = "lucee";
-    #   repo = "lucee-installer";
-    #   rev = "da57a8ec6e2993ed873445dde2ce4ea8dc2c4bb8";
-    #   sha256 = "sha256-KqdDwzSy1r9jiIMVRJHyLaQgsdUk75TIOI9LsSISOII=";
-    # }}/lucee/tomcat9/tomcat-lucee-conf"];
   };
 }
 
