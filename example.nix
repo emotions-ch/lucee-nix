06e3e3ab62b0aceae476d55e0fc60e2e62fad112
Ori <meow@ori.cat>
2026-02-02 19:14:51 +0100

    convert config files to dreivations

--- example.nix
+++ example.nix
@@ -18,20 +18,40 @@ let
     sha256 = "0ppizzx1q2lpfl8mn3bhaf145dj38pmj0cpgax21jx68fcy98m8s";
   };
 
-  contextXml = pkgs.fetchurl {
-    url = "https://github.com/lucee/lucee-installer/raw/0d5e993ba99aa7ce4b64b1d26e73e250b9b30a3b/lucee/tomcat9/tomcat-lucee-conf/conf/context.xml"; #context.xml from lucee7 installer
-    sha256 = "04ldddb8pq7y3y5yggz29ghq9i08a8jgs4v12is0hhppm9b23hir";
+  contextXml = pkgs.stdenv.mkDerivation {
+    name = "lucee-contextXml";
+    src = pkgs.fetchurl {
+      url = "https://github.com/lucee/lucee-installer/raw/0d5e993ba99aa7ce4b64b1d26e73e250b9b30a3b/lucee/tomcat9/tomcat-lucee-conf/conf/context.xml"; #context.xml from lucee7 installer
+      sha256 = "04ldddb8pq7y3y5yggz29ghq9i08a8jgs4v12is0hhppm9b23hir";
+    };
+
+    dontUnpack = true;
+
+    installPhase = ''
+      mkdir -p $out
+      cp $src $out/context.xml
+    '';
   };
 
-  webXml = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/lucee/lucee-installer/a22896f9b0caef10ea19943ba6302f2499bda96c/lucee/tomcat9/tomcat-lucee-conf/conf/web.xml"; #web.xml from lucee7 installer
-    sha256 = "1kxg7yymi7gyzf3pa6yn521d86zmgn9mivy2skpn3hbfaa0qar9m";
+  webXml = pkgs.stdenv.mkDerivation {
+    name = "lucee-webXml";
+    src = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/lucee/lucee-installer/a22896f9b0caef10ea19943ba6302f2499bda96c/lucee/tomcat9/tomcat-lucee-conf/conf/web.xml"; #web.xml from lucee7 installer
+      sha256 = "1kxg7yymi7gyzf3pa6yn521d86zmgn9mivy2skpn3hbfaa0qar9m";
+    };
+
+    dontUnpack = true;
+
+    installPhase = ''
+      mkdir -p $out
+      cp $src $out/web.xml
+    '';
   };
 
   catalinaProperties = pkgs.stdenv.mkDerivation {
     # this is a derivation because we are overriding the default catalina.properties which does get modified in the service config so we need to apply the same modifications to ourt
     # https://github.com/NixOS/nixpkgs/blob/ed142ab1b3a092c4d149245d0c4126a5d7ea00b0/nixos/modules/services/web-servers/tomcat.nix#L237C1-L241C87 
-    name = "catalinaProperties-service-patch";
+    name = "lucee-catalinaProperties-service-patch";
     src = pkgs.fetchurl {
       url = "https://github.com/lucee/lucee-installer/raw/860059147b84686b2d3db681cc30f216bc17c4f7/lucee/tomcat10/conf/catalina.properties"; # catalina.properties from lucee7 installer
       sha256 = "1i9vbgg39gxzgn4zw3gbm5apfqchksvqssna1dg2jbd253xydllx";
@@ -41,11 +61,12 @@ let
     buildInputs = [ pkgs.gnused ];
 
     installPhase = ''
+      mkdir -p $out
       # Create a modified catalina.properties file
       # Change all references from CATALINA_HOME to CATALINA_BASE and add support for shared libraries
       sed -e 's|''${catalina.home}|''${catalina.base}|g' \
         -e 's|shared.loader=|shared.loader=''${catalina.base}/shared/lib/*.jar|' \
-        $src > $out
+        $src > $out/catalina.properties
     '';
   };
 in
@@ -58,9 +79,9 @@ in
 
     serverXml = builtins.readFile serverXml;
     extraConfigFiles = [
-      contextXml
-      webXml
-      catalinaProperties
+      "${contextXml}/context.xml"
+      "${webXml}/web.xml"
+      "${catalinaProperties}/catalina.properties"
     ];
 
     webapps = ["${pkgs.fetchFromGitHub {
