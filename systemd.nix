21df084cdb4f034edeb82d51ac831c6c2a7ade7e
Ori <meow@ori.cat>
2026-02-03 11:57:23 +0100

    clean lucee-dir after tomcat stops

--- systemd.nix
+++ systemd.nix
@@ -11,6 +11,15 @@ in
         preStart = lib.mkAfter ''
           ln -sfn ${config.services.tomcat.package}/lucee ${config.services.tomcat.baseDir}
         '';
+
+        postStop = lib.mkAfter (if config.services.tomcat.purifyOnStart then ''
+            ${pkgs.coreutils}/bin/rm -rf ${lucee-dir}/*
+            ${pkgs.coreutils}/bin/find ${lucee-dir} -mindepth 1 -delete 2>/dev/null || true
+
+            ${pkgs.coreutils}/bin/mkdir -p ${lucee-dir}
+            ${pkgs.coreutils}/bin/chmod 0755 ${lucee-dir}
+            ${pkgs.coreutils}/bin/chown -R ${config.services.tomcat.user}:${config.services.tomcat.group} ${lucee-dir}
+        '' else "");
       };
 
       "lucee-setup" = {
@@ -50,7 +59,7 @@ in
     tmpfiles.rules = [
       # Format: d <path> <mode> <user> <group> <age or ->
       "d ${lucee-dir} 0755 ${config.services.tomcat.user} ${config.services.tomcat.group} -"
-      "d ${lucee-dir}/deploy 0755 ${config.services.tomcat.user} ${config.services.tomcat.group} -"
+      "d ${lucee-dir}/server/lucee-server/deploy 0755 ${config.services.tomcat.user} ${config.services.tomcat.group} -"
     ];
   };
 }
