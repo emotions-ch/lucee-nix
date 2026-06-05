788c5dc504616c5ce18e37b83b3e2579633f8c12
GitHub Action <action@github.com>
2026-06-05 00:32:37 +0000

    chore: update Lucee definitions
    - Updated Lucee version definitions automatically
    - Generated by scheduled workflow run on 2026-06-05 00:32:37 UTC

--- extensions/definitions.nix
+++ extensions/definitions.nix
@@ -1,5 +1,5 @@
 # Lucee Extensions Definitions - Auto-generated
-# Generated at: 2026-06-04 00:40:36 UTC
+# Generated at: 2026-06-05 00:32:36 UTC
 # DO NOT EDIT MANUALLY - Use lucee-updater tool
 
 { mkLuceeExtension }:
--- lucee/definitions.nix
+++ lucee/definitions.nix
@@ -1,5 +1,5 @@
 # Lucee Definitions - Auto-generated
-# Generated at: 2026-06-04 00:40:36 UTC
+# Generated at: 2026-06-05 00:32:36 UTC
 # DO NOT EDIT MANUALLY - Use lucee-updater tool
 
 {
@@ -40,49 +40,24 @@
   lucee7-zero = mkLuceeVersion {
     name = "lucee-zero";
     description = "Lucee Jar file without any Extensions bundled or doc and admin bundles, \"Lucee zero\"";
-    version = "7.0.3.43";
-    sha256 = "sha256-DQ8USIzieeVwIkw6ud4pcNcupG+Wk4+DpNAbLFZCvOE=";
+    version = "7.0.4.34";
+    sha256 = "sha256-EgIAzyjWlAwZz7+rdBk3naS3/+QLUzSGClOaagy+8bc=";
     javaVersion = 25;
   };
 
   lucee7 = mkLuceeVersion {
     name = "lucee";
     description = "Lucee jar file without dependencies Lucee needs to run";
-    version = "7.0.3.43";
-    sha256 = "sha256-55FO6hLPGHjqDB0JBhWYQ1tls3Cb+bHz8p1dVA9kiXo=";
+    version = "7.0.4.34";
+    sha256 = "sha256-pFM/Ilt1+lFDAYD+oiugv2Vzx/py7QiwusFleM6Z6Rc=";
     javaVersion = 25;
   };
 
   lucee7-light = mkLuceeVersion {
     name = "lucee-light";
     description = "Lucee Jar file without any Extensions bundled, \"Lucee light\"";
-    version = "7.0.3.43";
-    sha256 = "sha256-WbWWz5VGnrf1dPSf8XU0/ktzdpp+rf+ZWv7MmNSN8pQ=";
-    javaVersion = 25;
-  };
-
-
-  lucee7_0-RC-zero = mkLuceeVersion {
-    name = "lucee-zero";
-    description = "Lucee Jar file without any Extensions bundled or doc and admin bundles, \"Lucee zero\"";
-    version = "7.0.4.34-RC";
-    sha256 = "sha256-I3/Ow7Za1FwrM0+6r+baflmcWCbGzTM4PdcODQsleq8=";
-    javaVersion = 25;
-  };
-
-  lucee7_0-RC = mkLuceeVersion {
-    name = "lucee";
-    description = "Lucee jar file without dependencies Lucee needs to run";
-    version = "7.0.4.34-RC";
-    sha256 = "sha256-ABR2m21zHSF7dH0gDEI8OKJW1jR2vHiSB99j1B2VSgo=";
-    javaVersion = 25;
-  };
-
-  lucee7_0-RC-light = mkLuceeVersion {
-    name = "lucee-light";
-    description = "Lucee Jar file without any Extensions bundled, \"Lucee light\"";
-    version = "7.0.4.34-RC";
-    sha256 = "sha256-q4Igi7xdqQd6sPxP/gPx4M1u0uk3ysdUtCjSXsnrCW4=";
+    version = "7.0.4.34";
+    sha256 = "sha256-eN6IWCkn8nmZamJe4YorTjIoIQUZT57j1YgXcIiLTcE=";
     javaVersion = 25;
   };
 
@@ -90,49 +65,24 @@
   lucee6-zero = mkLuceeVersion {
     name = "lucee-zero";
     description = "Lucee Jar file without any Extensions bundled or doc and admin bundles, \"Lucee zero\"";
-    version = "6.2.6.19";
-    sha256 = "sha256-GbFTGm/vPMLzghRD3VKUckUBVR5/AuuxX++LSS1xoOM=";
+    version = "6.2.7.16";
+    sha256 = "sha256-GpWsX1fej49TuX29tZMUKqFLmDtfgXwLEM5Epzm/r9I=";
     javaVersion = 25;
   };
 
   lucee6 = mkLuceeVersion {
     name = "lucee";
     description = "Lucee jar file without dependencies Lucee needs to run";
-    version = "6.2.6.19";
-    sha256 = "sha256-RfUAnTbFl1LX89zFRBjiz99GN5OJmRXOWpy6FQ5S/dI=";
+    version = "6.2.7.16";
+    sha256 = "sha256-l+5litC6tJJWr7b7pQKD3HG7mtDsZzNAAv29O7AE+2g=";
     javaVersion = 25;
   };
 
   lucee6-light = mkLuceeVersion {
     name = "lucee-light";
     description = "Lucee Jar file without any Extensions bundled, \"Lucee light\"";
-    version = "6.2.6.19";
-    sha256 = "sha256-ZHbpfayd5E8RyKmTQgXnbGuFNe3uVS1Lr1aZ3pbaEA4=";
-    javaVersion = 25;
-  };
-
-
-  lucee6_2-RC-zero = mkLuceeVersion {
-    name = "lucee-zero";
-    description = "Lucee Jar file without any Extensions bundled or doc and admin bundles, \"Lucee zero\"";
-    version = "6.2.7.16-RC";
-    sha256 = "sha256-r+DvWvD5RATGEntlbMuYwWTPbZHom+7sPFkrGJEhhiU=";
-    javaVersion = 25;
-  };
-
-  lucee6_2-RC = mkLuceeVersion {
-    name = "lucee";
-    description = "Lucee jar file without dependencies Lucee needs to run";
-    version = "6.2.7.16-RC";
-    sha256 = "sha256-dpNxUsTcWQseyt7nnug8KyLoOr8YWYSmg4lWvOYztMo=";
-    javaVersion = 25;
-  };
-
-  lucee6_2-RC-light = mkLuceeVersion {
-    name = "lucee-light";
-    description = "Lucee Jar file without any Extensions bundled, \"Lucee light\"";
-    version = "6.2.7.16-RC";
-    sha256 = "sha256-sqark9w1OMwhMdzudN58pzla1E4ymQkQkaNOgvvNNG4=";
+    version = "6.2.7.16";
+    sha256 = "sha256-W2kPWzmrub48XGYiBA4+ukeNiCdjsI2vw5FVPQ6F0Ec=";
     javaVersion = 25;
   };
 
