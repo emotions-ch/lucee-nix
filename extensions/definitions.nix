42c89eda2b96fb8468a3404e1b2632c3d29eb904
GitHub Action <action@github.com>
2026-07-22 00:21:51 +0000

    chore: update Lucee definitions
    - Updated Lucee version definitions automatically
    - Generated by scheduled workflow run on 2026-07-22 00:21:51 UTC

--- extensions/definitions.nix
+++ extensions/definitions.nix
@@ -1,5 +1,5 @@
 # Lucee Extensions Definitions - Auto-generated
-# Generated at: 2026-07-21 00:23:23 UTC
+# Generated at: 2026-07-22 00:21:51 UTC
 # DO NOT EDIT MANUALLY - Use lucee-updater tool
 
 { mkLuceeExtension }:
