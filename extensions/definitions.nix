cdbfb1a89c9a973f89198acfb4517b49159d63a7
Ori <meow@ori.cat>
2026-08-06 14:52:22 +0200

    lucee updater broke, pinned to last sucsessful fetch

--- extensions/definitions.nix
+++ extensions/definitions.nix
@@ -1,10 +1,578 @@
 # Lucee Extensions Definitions - Auto-generated
-# Generated at: 2026-08-06 00:20:36 UTC
+# Generated at: 2026-07-02 00:31:13 UTC
 # DO NOT EDIT MANUALLY - Use lucee-updater tool
 
 { mkLuceeExtension }:
 
 {
+
+  administrator_extension = mkLuceeExtension {
+    name = "administrator-extension";
+    description = "Lucee administrator extension";
+    version = "1.0.0.7";
+    sha256 = "sha256-/sAER5PB6IBU/FQW48NrqylgSGnHnADzECejcbeK6Zg=";
+  };
+
+
+  ajax_extension = mkLuceeExtension {
+    name = "ajax.extension";
+    description = "Lucee ajax extension";
+    version = "1.0.0.7";
+    sha256 = "sha256-qNQTeBSYlTrnplFrBDx4DrO1cBGJQS0oWmuBTGO/5dw=";
+  };
+
+
+  argon2_extension = mkLuceeExtension {
+    name = "argon2-extension";
+    description = "Lucee argon2 extension";
+    version = "1.0.0.10";
+    sha256 = "sha256-HVrvkXQXe0CVdlJ0YM9e49dMx2mw3ODR0L81BFrEZBE=";
+  };
+
+
+  cfspreadsheet = mkLuceeExtension {
+    name = "cfspreadsheet";
+    description = "Lucee cfspreadsheet extension";
+    version = "3.0.5";
+    sha256 = "sha256-iFAOoSNRy+JFMClQ54EyyQDu/HyadxK5G9h14fbJfZ4=";
+  };
+
+
+  chart_extension = mkLuceeExtension {
+    name = "chart-extension";
+    description = "Lucee chart extension";
+    version = "2.0.0.1";
+    sha256 = "sha256-zKprrFbwMAYRkWQQcfPCtb2XNYdxRjAxLMZzwUVqny8=";
+  };
+
+
+  com_microsoft_sqlserver_mssql_jdbc = mkLuceeExtension {
+    name = "com.microsoft.sqlserver.mssql-jdbc";
+    description = "Lucee com microsoft sqlserver mssql jdbc extension";
+    version = "6.5.4";
+    sha256 = "sha256-zoqed8b4rMV7SIKp4YozQ47PQzGPD0M/rO2cj5ke+yk=";
+  };
+
+
+  com_mysql_cj = mkLuceeExtension {
+    name = "com.mysql.cj";
+    description = "Lucee com mysql cj extension";
+    version = "9.5.0";
+    sha256 = "sha256-0OAVoTR03Hkltx8RhpV0kKVuk4DDlCWjEBmIBu9wmGY=";
+  };
+
+
+  com_mysql_jdbc = mkLuceeExtension {
+    name = "com.mysql.jdbc";
+    description = "Lucee com mysql jdbc extension";
+    version = "5.1.40";
+    sha256 = "sha256-wo7vcPV61M+0YOM00F1dhVMphTcv1bW7iVD9xhNEOU4=";
+  };
+
+
+  com_teradata_jdbc = mkLuceeExtension {
+    name = "com.teradata.jdbc";
+    description = "Lucee com teradata jdbc extension";
+    version = "16.20.0.12";
+    sha256 = "sha256-aJIodse7npXFT5XLf3u7ZP4mYbwTUFHLGeNVFYFsLg0=";
+  };
+
+
+  compress_extension = mkLuceeExtension {
+    name = "compress-extension";
+    description = "Lucee compress extension";
+    version = "2.0.0.3";
+    sha256 = "sha256-WQFCiHm0W2AGTGBmWvu+NTDmbZhrWxtqLVK+UVPIhdk=";
+  };
+
+
+  couchbase_extension = mkLuceeExtension {
+    name = "couchbase-extension";
+    description = "Lucee couchbase extension";
+    version = "1.0.0.37";
+    sha256 = "sha256-gCnSna4tuqOGN88St8L36BnBW65dscgdnpPb3MyjA48=";
+  };
+
+
+  crypto_extension = mkLuceeExtension {
+    name = "crypto-extension";
+    description = "Lucee crypto extension";
+    version = "1.0.0.1-SNAPSHOT";
+    sha256 = "sha256-e5iRQKWGtoOWGcTk0P4fCj1SFcPZFSndIgD15ezNtus=";
+  };
+
+
+  debugger_extension = mkLuceeExtension {
+    name = "debugger-extension";
+    description = "Lucee debugger extension";
+    version = "3.0.0.4";
+    sha256 = "sha256-roPd7kan3t+4HnsGPR/G52CpUBkSuTDuaXhLuQpAI+Y=";
+  };
+
+
+  documentation_extension = mkLuceeExtension {
+    name = "documentation-extension";
+    description = "Lucee documentation extension";
+    version = "1.0.0.6";
+    sha256 = "sha256-V8FRLmudDuaJ3YlicvHS6VQ0vD1NkAmbUF5I7kmz6Ww=";
+  };
+
+
+  dynamodb_extension = mkLuceeExtension {
+    name = "dynamodb-extension";
+    description = "Lucee dynamodb extension";
+    version = "1.0.0.8";
+    sha256 = "sha256-ylKa/l9pYKNOkQNx3i1nko/5nCYGrsJhQUaE0bdNciI=";
+  };
+
+
+  ec2_extension = mkLuceeExtension {
+    name = "ec2-extension";
+    description = "Lucee ec2 extension";
+    version = "1.0.0.5";
+    sha256 = "sha256-WBSZKzlVMUg+pwe0aTjar6hAQ0VVPyz2llrnVOShFls=";
+  };
+
+
+  ehcache_extension = mkLuceeExtension {
+    name = "ehcache-extension";
+    description = "Lucee ehcache extension";
+    version = "2.10.0.39";
+    sha256 = "sha256-nTFqTOKSPmriWp4wPlNRSIFKx3fOoXFh46rz40zd0/Q=";
+  };
+
+
+  esapi_extension = mkLuceeExtension {
+    name = "esapi-extension";
+    description = "Lucee esapi extension";
+    version = "3.0.0.14";
+    sha256 = "sha256-0LDQngDevbVoXsrqLzF9byndmJXhEnnAfanl9Z5OshQ=";
+  };
+
+
+  extension_infinispan = mkLuceeExtension {
+    name = "extension-infinispan";
+    description = "Lucee infinispan extension";
+    version = "8.2.3.21";
+    sha256 = "sha256-phxhVPLn0Lut6Tk7u04RDhEbMg+k7nnxe1Fj9xTFCFs=";
+  };
+
+
+  extension_json_extension = mkLuceeExtension {
+    name = "extension-json-extension";
+    description = "Lucee json extension";
+    version = "1.0.0.0-RC";
+    sha256 = "sha256-I3DrSW6JhwYnM9xaB2njMw8n1cRBl3JrHkLFbBAJNoc=";
+  };
+
+
+  extension_loganalyzer = mkLuceeExtension {
+    name = "extension-loganalyzer";
+    description = "Lucee loganalyzer extension";
+    version = "2.3.1.16";
+    sha256 = "sha256-Og/mC5Ak7n0Z8o5segWldFalXoZXHC1lzfeqa8N73Zk=";
+  };
+
+
+  extension_memcached = mkLuceeExtension {
+    name = "extension-memcached";
+    description = "Lucee memcached extension";
+    version = "3.0.2.29";
+    sha256 = "sha256-s4ZgdebhL21n9c3cvdj/agviQ/V67ogMYO/uhI/T7E0=";
+  };
+
+
+  extension_redissentinel = mkLuceeExtension {
+    name = "extension-redissentinel";
+    description = "Lucee redissentinel extension";
+    version = "2.7.2.1-BETA";
+    sha256 = "sha256-mdYgDmOGzOvOq3U5Htw+rY90Vis0GhDUQUGNjw9HN6E=";
+  };
+
+
+  form_extension = mkLuceeExtension {
+    name = "form-extension";
+    description = "Lucee form extension";
+    version = "2.0.0.7";
+    sha256 = "sha256-9fKeaO6FRMOJxNH0PY/D+q4c7Kfe1fqHRcyq7NL0kX4=";
+  };
+
+
+  git_extension = mkLuceeExtension {
+    name = "git-extension";
+    description = "Lucee git extension";
+    version = "4.11.0.36";
+    sha256 = "sha256-tvtnjW2YOUZMznVKGgZrRoT8maSPLNRmBpWmWalhnGI=";
+  };
+
+
+  hibernate_extension = mkLuceeExtension {
+    name = "hibernate-extension";
+    description = "Lucee hibernate extension";
+    version = "5.6.15.15-RC";
+    sha256 = "sha256-snEl237RMU7GEr+d68/RUwpDejVspGp7V6L0J0P819U=";
+  };
+
+
+  hibernate_orm = mkLuceeExtension {
+    name = "hibernate-orm";
+    description = "Lucee hibernate orm extension";
+    version = "3.5.5.89";
+    sha256 = "sha256-Bp4wueJm9Cx2qldiQdla14bs0F43zVaAm64bRnXSC2A=";
+  };
+
+
+  hypersonic_hsqldb = mkLuceeExtension {
+    name = "hypersonic.hsqldb";
+    description = "Lucee hypersonic hsqldb extension";
+    version = "1.8.0";
+    sha256 = "sha256-tc4dUFa/SPN2p6/bKvwHWwz7mJbYvfUQrplcmPkoUK8=";
+  };
+
+
+  image_extension = mkLuceeExtension {
+    name = "image-extension";
+    description = "Lucee image extension";
+    version = "3.0.1.1";
+    sha256 = "sha256-Yln6kGfQ2TTA0VWNe+5aGd7QivLZZGcrGDTXnYUFAHI=";
+  };
+
+
+  json_extension = mkLuceeExtension {
+    name = "json-extension";
+    description = "Lucee json extension";
+    version = "2.0.0.3";
+    sha256 = "sha256-G5kJj7Pw4zLjxRahoj55BJJz/mUMcPfRRBBAasvWKlY=";
+  };
+
+
+  jsonata_extension = mkLuceeExtension {
+    name = "jsonata-extension";
+    description = "Lucee jsonata extension";
+    version = "1.0.0.0-SNAPSHOT";
+    sha256 = "sha256-e9BCRfc0OVTJCpqXZpghvaLJUr283VwtsU5BBl+Y5cE=";
+  };
+
+
+  jtds = mkLuceeExtension {
+    name = "jtds";
+    description = "Lucee jtds extension";
+    version = "1.3.1";
+    sha256 = "sha256-XPsEjpvcGQXE9Gv77Lus9fRn8MfJbxmeABaqaN9u2Cw=";
+  };
+
+
+  kinesis_extension = mkLuceeExtension {
+    name = "kinesis-extension";
+    description = "Lucee kinesis extension";
+    version = "1.0.1.3";
+    sha256 = "sha256-w7w6ySl2jR+X7buAkF/qxX1ZsCwtMthhcQLUelzY2/I=";
+  };
+
+
+  lucee_admin_extension = mkLuceeExtension {
+    name = "lucee.admin.extension";
+    description = "Lucee lucee admin extension";
+    version = "1.0.0.5";
+    sha256 = "sha256-YqLZ5HPjLPul5KLBqCB6E1wjZmQTdJOApoy6Aib7f2E=";
+  };
+
+
+  lucee_doc_extension = mkLuceeExtension {
+    name = "lucee.doc.extension";
+    description = "Lucee lucee doc extension";
+    version = "1.0.0.4";
+    sha256 = "sha256-pCaXn1R5mUH8BG1ImnJz4ll0L31s1GF7e0qDC+TY+W4=";
+  };
+
+
+  lucee_extension_couchbase = mkLuceeExtension {
+    name = "lucee.extension.couchbase";
+    description = "Lucee lucee  couchbase extension";
+    version = "1.0.0.35-SNAPSHOT";
+    sha256 = "sha256-fRwhyyVH61K1O8j5jFE0p+Zs+25o++nIAc6tNWgvEiY=";
+  };
+
+
+  lucee_image_extension = mkLuceeExtension {
+    name = "lucee.image.extension";
+    description = "Lucee lucee image extension";
+    version = "2.0.0.34";
+    sha256 = "sha256-azVKpNcAYJVJGb4ZzAYwQu9Rv51Xu+nOXnYQnNgKE5I=";
+  };
+
+
+  lucene_search_extension = mkLuceeExtension {
+    name = "lucene-search";
+    description = "Lucee lucene search extension";
+    version = "3.0.0.163";
+    sha256 = "sha256-OqhwnD63QdHW8Ay4xwIwBcOMZOgMEgIrK2F+PI/lXPw=";
+  };
+
+
+  mail_extension = mkLuceeExtension {
+    name = "mail-extension";
+    description = "Lucee mail extension";
+    version = "1.1.0.6";
+    sha256 = "sha256-/K6gGUO+MTDBBe7vJCBkH0U5ShbxVm63V0XlSEXxg5Y=";
+  };
+
+
+  mcp_server_extension = mkLuceeExtension {
+    name = "mcp-server-extension";
+    description = "Lucee mcp server extension";
+    version = "1.0.0.9-RC";
+    sha256 = "sha256-5JuKxUe9XTQG3BiaJuceqSekPfDiqi1SSv1x5RilEEo=";
+  };
+
+
+  memcached_extension = mkLuceeExtension {
+    name = "memcached-extension";
+    description = "Lucee memcached extension";
+    version = "4.0.0.14";
+    sha256 = "sha256-Imy7qqA11ZEceSRIOYmnNl+as7i/5nHRUk2meqT9/ts=";
+  };
+
+
+  microsoft_sqljdbc = mkLuceeExtension {
+    name = "microsoft.sqljdbc";
+    description = "Lucee microsoft sqljdbc extension";
+    version = "4.0.0";
+    sha256 = "sha256-GIPliBrHAuwvA8gVhM2e7SOsCFT3rCf8h5+SE0ck2eY=";
+  };
+
+
+  mongodb_extension = mkLuceeExtension {
+    name = "mongodb-extension";
+    description = "Lucee mongodb extension";
+    version = "3.12.13.10";
+    sha256 = "sha256-2dLOQSnsryF6NSHm8Pc7YAkPifEFyOQ5BzOQeX8nnJE=";
+  };
+
+
+  mssqljdbc4 = mkLuceeExtension {
+    name = "mssqljdbc4";
+    description = "Lucee mssqljdbc4 extension";
+    version = "6.0.7728.100";
+    sha256 = "sha256-2h6g8bVk+vL9JrpybROFC/GNDa1LbmGqzG1qeQP6WDA=";
+  };
+
+
+  mysql_jdbc_extension = mkLuceeExtension {
+    name = "mysql-jdbc-extension";
+    description = "Lucee mysql jdbc extension";
+    version = "9.6.0";
+    sha256 = "sha256-Ih3DuP+jCfUyNf1LjiT/Gi0sBOOZ5ON4wavFFReUuR8=";
+  };
+
+
+  org_duckdb_duckdb_jdbc = mkLuceeExtension {
+    name = "org.duckdb.duckdb_jdbc";
+    description = "Lucee org duckdb duckdb jdbc extension";
+    version = "1.3.2.1";
+    sha256 = "sha256-NSkN7LvF297ngp5JeIA23heTfA1wYkXYW9X+dp5oPBI=";
+  };
+
+
+  org_h2 = mkLuceeExtension {
+    name = "org.h2";
+    description = "Lucee org h2 extension";
+    version = "1.3.172";
+    sha256 = "sha256-e+0yOsyvJGGmTczlQckP+RbhKzBYHeKyLbSOxbReQI4=";
+  };
+
+
+  org_hsqldb_hsqldb = mkLuceeExtension {
+    name = "org.hsqldb.hsqldb";
+    description = "Lucee org hsqldb hsqldb extension";
+    version = "2.4.0";
+    sha256 = "sha256-1/XleNo0wcef8rSafvA3RkQ+Gn+s2actsUarTowR/Bc=";
+  };
+
+
+  org_lucee_athena_jdbc = mkLuceeExtension {
+    name = "org.lucee.athena.jdbc";
+    description = "Lucee org lucee athena jdbc extension";
+    version = "3.2.2.2";
+    sha256 = "sha256-Z1iV0pUlmsPmaS/24J4qva2n8i/tQoTCxFnKCbGvXsA=";
+  };
+
+
+  org_lucee_axis_extension = mkLuceeExtension {
+    name = "org.lucee.axis.extension";
+    description = "Lucee org lucee axis extension";
+    version = "1.5.0.6";
+    sha256 = "sha256-xuhl5eUZA0b92odsT+YWSf+XAe4X/G7gMGnSYPtD5Xw=";
+  };
+
+
+  org_lucee_exasol = mkLuceeExtension {
+    name = "org.lucee.exasol";
+    description = "Lucee org lucee exasol extension";
+    version = "25.2.5";
+    sha256 = "sha256-bii63PgjQN9VQ86v5ABE0EDly7jpAhmdip59dkTylO8=";
+  };
+
+
+  org_lucee_h2 = mkLuceeExtension {
+    name = "org.lucee.h2";
+    description = "Lucee org lucee h2 extension";
+    version = "2.1.214.0001L";
+    sha256 = "sha256-WP2aEj4Y3gwtRs7mZ88mxSs4jPZBebZu0NuPXDc+7vA=";
+  };
+
+
+  org_lucee_hsqldb = mkLuceeExtension {
+    name = "org.lucee.hsqldb";
+    description = "Lucee org lucee hsqldb extension";
+    version = "2.7.2.jdk11";
+    sha256 = "sha256-Pi+eAntouuQhQoKmF9oUO7RKuef+G/GQ/SP7e1KgIWY=";
+  };
+
+
+  org_lucee_mssql = mkLuceeExtension {
+    name = "org.lucee.mssql";
+    description = "Lucee org lucee mssql extension";
+    version = "13.2.1";
+    sha256 = "sha256-DnOLGsAjwZrxE0034843NQXR1NzsCrI6ziMelT3BB3I=";
+  };
+
+
+  org_lucee_oracle = mkLuceeExtension {
+    name = "org.lucee.oracle";
+    description = "Lucee org lucee oracle extension";
+    version = "23.26.0.0-ojdbc11";
+    sha256 = "sha256-SldbO66VBauRb55ukoAs4XQOfRE6N/0ck60a0VwpxQM=";
+  };
+
+
+  org_lucee_postgresql = mkLuceeExtension {
+    name = "org.lucee.postgresql";
+    description = "Lucee org lucee postgresql extension";
+    version = "8.3.0.jdbc4";
+    sha256 = "sha256-2h9U8t5Zr3i6I+IBDWZpwGJgYRGjVEe1RqGMIFrIw6A=";
+  };
+
+
+  org_postgresql_jdbc = mkLuceeExtension {
+    name = "org.postgresql.jdbc";
+    description = "Lucee org postgresql jdbc extension";
+    version = "42.7.7";
+    sha256 = "sha256-lMTcasIw7qWQ73LMDKrrFvd4R4T1Um2nGcVh/qywoHk=";
+  };
+
+
+  org_postgresql_jdbc42 = mkLuceeExtension {
+    name = "org.postgresql.jdbc42";
+    description = "Lucee org postgresql jdbc42 extension";
+    version = "42.1.4";
+    sha256 = "sha256-cVVt6xNG2iNDmldZ0p/MAAb6GkgYDG6gNMmxDxrIAWU=";
+  };
+
+
+  pdf_extension = mkLuceeExtension {
+    name = "pdf-extension";
+    description = "Lucee pdf extension";
+    version = "2.0.1.0";
+    sha256 = "sha256-0TDI3FEDt81cYGmqILQiJGetMzT/ebhMcF5A+Lhr3N4=";
+  };
+
+
+  poi_extension = mkLuceeExtension {
+    name = "poi-extension";
+    description = "Lucee poi extension";
+    version = "1.0.0.7";
+    sha256 = "sha256-puRQefecYcgOcHSB6Eqc69+6VNG8KBjHwluYIL1j4hI=";
+  };
+
+
+  postgresql_jdbc_extension = mkLuceeExtension {
+    name = "postgresql-jdbc-extension";
+    description = "Lucee postgresql jdbc extension";
+    version = "42.7.9";
+    sha256 = "sha256-tLpKEQDLuHtEEb3sT1xQG37t16VMSKX//d6vtnHGQD8=";
+  };
+
+
+  quartz_extension = mkLuceeExtension {
+    name = "quartz-extension";
+    description = "Lucee quartz extension";
+    version = "1.0.0.53";
+    sha256 = "sha256-jPWbsguGaM+tV6L8r8xXpI5R+7AAfvbK0sHt/v43Dyc=";
+  };
+
+
+  redis_extension = mkLuceeExtension {
+    name = "redis.extension";
+    description = "Lucee redis extension";
+    version = "4.0.0.2";
+    sha256 = "sha256-i+Cvr7198x535vj0XJKUD9xK4E7k3P2NSqlXR1U0dcs=";
+  };
+
+
+  s3_extension = mkLuceeExtension {
+    name = "s3-extension";
+    description = "Lucee s3 extension";
+    version = "2.0.3.1";
+    sha256 = "sha256-6SnOn53FugffVLZ4fN94BdcLMVf21YZJgm54SDMLWNs=";
+  };
+
+
+  sapdbc = mkLuceeExtension {
+    name = "sapdbc";
+    description = "Lucee sapdbc extension";
+    version = "7.4.4";
+    sha256 = "sha256-bA1rF+pPTG97Rf7pKw9fZc/DvxQuLt7OY7gB8EA6mHM=";
+  };
+
+
+  scheduler_classic_extension = mkLuceeExtension {
+    name = "scheduler-classic-extension";
+    description = "Lucee scheduler classic extension";
+    version = "1.0.0.1";
+    sha256 = "sha256-e2cwzDBdn7ENP4Q2ZLkIj3nRZEBt66ZTntiyU1GcPWU=";
+  };
+
+
+  sentry_extension = mkLuceeExtension {
+    name = "sentry-extension";
+    description = "Lucee sentry extension";
+    version = "5.6.0.0";
+    sha256 = "sha256-AQSpjFimxsNIXURsbYpManggV3Kfv6FKbQ7DtOaRGtY=";
+  };
+
+
+  tasks_extension = mkLuceeExtension {
+    name = "tasks-extension";
+    description = "Lucee tasks extension";
+    version = "1.1.0.7";
+    sha256 = "sha256-U4RgnxHWV3T30d9K36k1QWengQXC4wryeUZ+p3o2lOg=";
+  };
+
+
+  websocket_extension = mkLuceeExtension {
+    name = "websocket-extension";
+    description = "Lucee websocket extension";
+    version = "3.0.0.18";
+    sha256 = "sha256-vlHldiefrsdI4C25aft79ZCqL6Y2HY4siae2813ycPY=";
+  };
+
+
+  websocket_client_extension = mkLuceeExtension {
+    name = "websocket-client-extension";
+    description = "Lucee websocket client extension";
+    version = "2.3.0.7";
+    sha256 = "sha256-mdies6nbLqnQdzC3rGIj4N3EMBMp8ZFGkVg1fsz7u+o=";
+  };
+
+
+  yaml_extension = mkLuceeExtension {
+    name = "Yaml";
+    description = "Native YAML support for Lucee via SnakeYAML 2.5: read, write, parse, serialize, validate, merge and JSON interop.";
+    version = "2.5.0-BETA";
+    sha256 = "sha256-pT2d9kCjhjuwXdhK1CKgYprEkdIUSBxAIFDohI6YC+s=";
+  };
+
 
 
 }
--- lucee/definitions.nix
+++ lucee/definitions.nix
@@ -1,5 +1,5 @@
 # Lucee Definitions - Auto-generated
-# Generated at: 2026-08-06 00:20:36 UTC
+# Generated at: 2026-07-02 00:31:13 UTC
 # DO NOT EDIT MANUALLY - Use lucee-updater tool
 
 {
@@ -10,6 +10,81 @@
 }:
 
 {
+
+
+  lucee7_1-BETA-zero = mkLuceeVersion {
+    name = "lucee-zero";
+    description = "Lucee Jar file without any Extensions bundled or doc and admin bundles, \"Lucee zero\"";
+    version = "7.1.0.71-BETA";
+    sha256 = "sha256-nUHYevX4vksWYaZWofycuGBwe8rGur4VSmGzzR5oPTA=";
+    javaVersion = 25;
+  };
+
+  lucee7_1-BETA = mkLuceeVersion {
+    name = "lucee";
+    description = "Lucee jar file without dependencies Lucee needs to run";
+    version = "7.1.0.71-BETA";
+    sha256 = "sha256-XcJbUfaO+kG7t85q/02wAl4c1BX9rRgFuZVVTOko+fQ=";
+    javaVersion = 25;
+  };
+
+  lucee7_1-BETA-light = mkLuceeVersion {
+    name = "lucee-light";
+    description = "Lucee Jar file without any Extensions bundled, \"Lucee light\"";
+    version = "7.1.0.71-BETA";
+    sha256 = "sha256-nUQBuwrrKfDB6fruAKyWlxyE9w5H7xb3OFSKg7IRQ1A=";
+    javaVersion = 25;
+  };
+
+
+  lucee7-zero = mkLuceeVersion {
+    name = "lucee-zero";
+    description = "Lucee Jar file without any Extensions bundled or doc and admin bundles, \"Lucee zero\"";
+    version = "7.0.4.34";
+    sha256 = "sha256-EgIAzyjWlAwZz7+rdBk3naS3/+QLUzSGClOaagy+8bc=";
+    javaVersion = 25;
+  };
+
+  lucee7 = mkLuceeVersion {
+    name = "lucee";
+    description = "Lucee jar file without dependencies Lucee needs to run";
+    version = "7.0.4.34";
+    sha256 = "sha256-pFM/Ilt1+lFDAYD+oiugv2Vzx/py7QiwusFleM6Z6Rc=";
+    javaVersion = 25;
+  };
+
+  lucee7-light = mkLuceeVersion {
+    name = "lucee-light";
+    description = "Lucee Jar file without any Extensions bundled, \"Lucee light\"";
+    version = "7.0.4.34";
+    sha256 = "sha256-eN6IWCkn8nmZamJe4YorTjIoIQUZT57j1YgXcIiLTcE=";
+    javaVersion = 25;
+  };
+
+
+  lucee6-zero = mkLuceeVersion {
+    name = "lucee-zero";
+    description = "Lucee Jar file without any Extensions bundled or doc and admin bundles, \"Lucee zero\"";
+    version = "6.2.7.16";
+    sha256 = "sha256-GpWsX1fej49TuX29tZMUKqFLmDtfgXwLEM5Epzm/r9I=";
+    javaVersion = 25;
+  };
+
+  lucee6 = mkLuceeVersion {
+    name = "lucee";
+    description = "Lucee jar file without dependencies Lucee needs to run";
+    version = "6.2.7.16";
+    sha256 = "sha256-l+5litC6tJJWr7b7pQKD3HG7mtDsZzNAAv29O7AE+2g=";
+    javaVersion = 25;
+  };
+
+  lucee6-light = mkLuceeVersion {
+    name = "lucee-light";
+    description = "Lucee Jar file without any Extensions bundled, \"Lucee light\"";
+    version = "6.2.7.16";
+    sha256 = "sha256-W2kPWzmrub48XGYiBA4+ukeNiCdjsI2vw5FVPQ6F0Ec=";
+    javaVersion = 25;
+  };
 
 
 
